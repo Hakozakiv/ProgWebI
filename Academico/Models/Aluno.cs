023c7bb3d25e7b482177ec72331023a69dc2f574
Victor_Hakozaki <victorhakozaki16@gmail.com>
2026-02-26 22:09:38 -0400

    :sparkes: criando aluno

---
+++ Academico/Models/Aluno.cs
@@ -0,0 +1,8 @@
+namespace Academico.Models;
+public class Aluno
+{
+    public string nome {get; set; }
+    public string matricula {get; set; }
+    public string cpf {get; set; }
+    public DateOnly dataNascimento {get; set; }
+}
